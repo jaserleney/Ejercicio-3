1e049f631d0f70efe5279e87baca77887f7609ff
Jasser Blanco <yaser.keny@gmail.com>
2023-01-29 20:42:23 -0500

    Terminando maquetacion estructura basica HTML hora 8:42pm

--- main.js
+++ main.js
@@ -2,5 +2,22 @@ var app = new Vue({
   el: "#app",
   data: {
     message: "Hello Vue!",
+    db: [
+      { user: "admin", password: 1234 },
+      { user: "secretario", password: 2345 },
+      { user: "vendedor", password: 6789 },
+      { user: "ensamblador", password: 0987 },
+    ],
+    selected: null,
+    password: null,
+    is: [
+      { isAdmin: false },
+      { isSecretario: false },
+      { isVendedor: false },
+      { isEnsamblador: false },
+    ],
+  },
+  methods: {
+    login() {},
   },
 });
