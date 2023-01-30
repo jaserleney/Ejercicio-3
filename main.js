2da01dd2d38261aacffd29b672bb56597f22e764
Jasser Blanco <yaser.keny@gmail.com>
2023-01-30 09:32:13 -0500

    Iniciando Estilos Maquetacion CSS hora 9:32am

--- main.js
+++ main.js
@@ -5,19 +5,82 @@ var app = new Vue({
     db: [
       { user: "admin", password: 1234 },
       { user: "secretario", password: 2345 },
-      { user: "vendedor", password: 6789 },
-      { user: "ensamblador", password: 0987 },
+      { user: "vendedor", password: 3456 },
+      { user: "ensamblador", password: 4567 },
     ],
     selected: null,
     password: null,
-    is: [
-      { isAdmin: false },
-      { isSecretario: false },
-      { isVendedor: false },
-      { isEnsamblador: false },
-    ],
+    is: {
+      isAdmin: false,
+      isSecretario: false,
+      isVendedor: false,
+      isEnsamblador: false,
+    },
   },
   methods: {
-    login() {},
+    login() {
+      if (
+        this.selected === null ||
+        this.password === null ||
+        this.selected === "" ||
+        this.password === "" ||
+        this.selected === undefined ||
+        this.password === undefined
+      ) {
+        alert("Debes seleccionar un selected y escribir una contraseña");
+      }
+
+      if (
+        this.selected === this.db[0].user &&
+        parseInt(this.password) === this.db[0].password
+      ) {
+        this.is = {
+          isAdmin: true,
+          isSecretario: false,
+          isVendedor: false,
+          isEnsamblador: false,
+        };
+        this.message = `Bienvenido! ${this.selected}`;
+      }
+
+      if (
+        this.selected === this.db[1].user &&
+        parseInt(this.password) === this.db[1].password
+      ) {
+        this.is = {
+          isAdmin: false,
+          isSecretario: true,
+          isVendedor: false,
+          isEnsamblador: false,
+        };
+        this.message = `Bienvenido! ${this.selected}`;
+      }
+
+      if (
+        this.selected === this.db[2].user &&
+        parseInt(this.password) === this.db[2].password
+      ) {
+        this.is = {
+          isAdmin: false,
+          isSecretario: false,
+          isVendedor: true,
+          isEnsamblador: false,
+        };
+        this.message = `Bienvenido! ${this.selected}`;
+      }
+
+      if (
+        this.selected === this.db[3].user &&
+        parseInt(this.password) === this.db[3].password
+      ) {
+        this.is = {
+          isAdmin: false,
+          isSecretario: false,
+          isVendedor: false,
+          isEnsamblador: true,
+        };
+        this.message = `Bienvenido! ${this.selected}`;
+      }
+    },
   },
 });
