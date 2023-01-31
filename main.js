6124b718076c2486b6d3ebe10e539f888fcd58a2
Jasser Blanco <yaser.keny@gmail.com>
2023-01-31 01:11:36 -0500

    Se agregaron funciones de las liquidaciones de ensamblador y secretario, asi como tambien agregue los archivos que sam aporto ya que tuvo dificultades para subirlo

--- main.js
+++ main.js
@@ -36,7 +36,16 @@ var app = new Vue({
     comisionVentas: "",
     salary: "",
     salaryBase: 1000000,
+
+    horasExtrasSecretario: "",
+    horasExtrasEnsamblador: "",
+
     button: true,
+
+    liquidacionSecretario: {},
+    liquidacionEnsamblador: {},
+
+    liquidaciones: [],
   },
 
   methods: {
@@ -268,6 +277,70 @@ var app = new Vue({
       this.comisionVentas = parseInt(this.comisionVentas);
       localStorage.setItem("comisionVentas", this.comisionVentas);
     },
+
+    calcHorasExtras() {
+      let valorHora = Math.round(this.salaryBase / 30 / 8);
+      const formatterPeso = new Intl.NumberFormat("es-CO", {
+        style: "currency",
+        currency: "COP",
+        minimumFractionDigits: 0,
+      });
+
+      if (this.horasExtrasSecretario && this.horasExtrasSecretario > 0) {
+        let valorHorasExtrasSecretario = Math.round(
+          valorHora * 1.8 * parseInt(this.horasExtrasSecretario)
+        );
+
+        this.liquidacionSecretario = {
+          empleado: "secretario",
+          liquidado: true,
+          salarioBase: formatterPeso.format(this.salaryBase),
+          valorHorasExtras: formatterPeso.format(valorHorasExtrasSecretario),
+          total: formatterPeso.format(
+            this.salaryBase + valorHorasExtrasSecretario
+          ),
+        };
+        this.liquidaciones.push(this.liquidacionSecretario);
+
+        localStorage.setItem(
+          "liquiSecretario",
+          JSON.stringify(this.liquidacionSecretario)
+        );
+        // console.log(this.liquidaciones);
+        localStorage.setItem(
+          "liquidaciones",
+          JSON.stringify(this.liquidaciones)
+        );
+      }
+
+      if (this.horasExtrasEnsamblador && this.horasExtrasEnsamblador > 0) {
+        let valorHorasExtrasEnsamblador = Math.round(
+          valorHora * 2.2 * parseInt(this.horasExtrasEnsamblador)
+        );
+
+        this.liquidacionEnsamblador = {
+          empleado: "ensamblador",
+          liquidado: true,
+          salarioBase: formatterPeso.format(this.salaryBase),
+          valorHorasExtras: formatterPeso.format(valorHorasExtrasEnsamblador),
+          total: formatterPeso.format(
+            this.salaryBase + valorHorasExtrasEnsamblador
+          ),
+        };
+
+        this.liquidaciones.push(this.liquidacionEnsamblador);
+
+        localStorage.setItem(
+          "liquiEnsamblador",
+          JSON.stringify(this.liquidacionEnsamblador)
+        );
+
+        localStorage.setItem(
+          "liquidaciones",
+          JSON.stringify(this.liquidaciones)
+        );
+      }
+    },
   },
 
   created() {
@@ -276,7 +349,10 @@ var app = new Vue({
       maxZap = localStorage.getItem("maxZap"),
       ensambleZto = localStorage.getItem("ensambleZto"),
       ensambleZap = localStorage.getItem("ensambleZap"),
-      comisionVentas = localStorage.getItem("comisionVentas");
+      comisionVentas = localStorage.getItem("comisionVentas"),
+      liquidaciones = localStorage.getItem("liquidaciones"),
+      liquiSecretario = localStorage.getItem("liquiSecretario"),
+      liquiEnsamblador = localStorage.getItem("liquiEnsamblador");
 
     if (maxZapatos !== null) {
       this.maxZapatos = parseInt(maxZapatos);
@@ -297,6 +373,15 @@ var app = new Vue({
     }
     if (comisionVentas !== null) {
       this.comisionVentas = parseInt(comisionVentas);
+    }
+    if (liquidaciones !== null) {
+      this.liquidaciones = JSON.parse(liquidaciones);
+    }
+    if (liquiSecretario !== null) {
+      this.liquidacionSecretario = JSON.parse(liquiSecretario);
+    }
+    if (liquiEnsamblador !== null) {
+      this.liquidacionEnsamblador = JSON.parse(liquiEnsamblador);
     }
   },
 });
